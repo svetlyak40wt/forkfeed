0625de54ce7bfacebeca4bc904f3ec636691a5f7
Alexander Artemenko <svetlyak.40wt@gmail.com>
2011-02-08 00:43:22 +0300

    Ability to go through all user's repositories.

--- fork2rss.py
+++ fork2rss.py
@@ -1,30 +1,67 @@
-#!/usr/bin/env python
+#!env/bin/python
 # -*- coding: utf-8 -*-
+from __future__ import with_statement
 
-import sys
+import logging
+import feedgenerator
 
+from opster import command
 from github2.client import Github
 
-def main(repository):
-    """Data processor.
-
-    Argument repos should be given in form username/repname.
-    """
+@command(usage='%name (username or username/repository) [-o output.xml]')
+def main(
+        repository_or_username,
+        output_filename=('o', '%(username)s-%(repository)s.xml', 'output filename'),
+    ):
+    """Utility to build Atom feed for new commits from your projects' forks on the GitHub."""
+    log = logging.getLogger()
     gh = Github()
 
-    # first, we need to get the date of the last commit
-    commits = gh.commits.list(repository, 'master')
-    last_commit_date = commits[0].committed_date
+    if '/' in repository_or_username:
+        username, repository = repository_or_username.split('/', 1)
+        repositories = [repository]
+    else:
+        username = repository_or_username
+        repositories = [rep.name for rep in gh.repos.list(username)]
 
-    for fork in gh.repos.network(repository):
-        fork_repname = '%(owner)s/%(name)s' % fork
-        fork_commits = gh.commits.list(fork_repname, 'master')
+    for repository in repositories:
+        full_repname = username + '/' + repository
+        log.info('Processing %s' % full_repname)
 
-        for commit in reversed(fork_commits):
-            if commit.committed_date > last_commit_date:
-                short_commit_message = commit.message.split('\n', 1)[0]
-                print fork_repname, commit.committed_date, short_commit_message
+        # first, we need to get the date of the last commit
+        commits = gh.commits.list(full_repname, 'master')
+        last_commit_date = commits[0].committed_date
+
+        feed = feedgenerator.Atom1Feed(
+            title=u'%s forks' % full_repname,
+            link=u'http://github.com/%s' % full_repname,
+            description=u'Commits from forks of %s repository at GitHub.' % full_repname,
+            language=u'en',
+        )
+
+        for fork in gh.repos.network(full_repname):
+            fork_repname = '%(owner)s/%(name)s' % fork
+
+            log.info('Fetching commits from %s' % fork_repname)
+            fork_commits = gh.commits.list(fork_repname, 'master')
+
+            for commit in reversed(fork_commits):
+                if commit.committed_date > last_commit_date:
+                    short_commit_message = commit.message.split('\n', 1)[0]
+                    log.debug('%s %s %s', fork_repname, commit.committed_date, short_commit_message)
+
+                    feed.add_item(
+                        title=fork_repname,
+                        link=u'http://github.com/%s/commit/%s' % (fork_repname, commit.id),
+                        description=short_commit_message,
+                    )
+
+        with open(output_filename % locals(), 'w') as f:
+            feed.write(f, 'utf-8')
 
 if __name__ == '__main__':
-    #main(sys.argv[1])
-    main('svetlyak40wt/cony')
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s %(message)s',
+        level=logging.DEBUG,
+    )
+    main()
